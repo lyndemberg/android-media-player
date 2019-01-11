a12702a6f24dcd3f02a796b89fde0e2ee876eeb0
lyndemberg <lyndembergbatista@outlook.com>
2019-01-11 17:22:28 -0300

    funcionando

---
+++ app/src/main/java/com/mesh/simplemetronome/MainActivity.java
@@ -0,0 +1,23 @@
+package com.mesh.simplemetronome;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mediaPlayer = MediaPlayer.create(this, R.raw.cat_meow);
+
+    }
+
+    public void play(View v){
+        mediaPlayer.start();
+    }
+}
